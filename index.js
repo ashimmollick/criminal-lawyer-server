2edcf78f95c76c9586e6f5fa539cbd117ac2bfeb
Ashim-mollick <ashimmollick46@gmail.com>
2022-11-10 07:50:38 +0600

    different service id added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -31,6 +31,13 @@ async function run() {
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray()
             res.send(services)
+        })
+        app.get('/details/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            const services = await serviceCollections.findOne(query)
+            res.send(service || services)
         })
     }
     finally {
