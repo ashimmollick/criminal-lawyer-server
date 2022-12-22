1007422a781ad98018268a311901acb8135d5b87
Ashim-mollick <ashimmollick46@gmail.com>
2022-12-22 11:47:56 +0600

    final code

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run() {
         app.delete('/review/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
-            const resulte = await orderCollection.deleteOne(query)
+            const resulte = await reviewCollections.deleteOne(query)
             res.send(resulte)
         })
     }
